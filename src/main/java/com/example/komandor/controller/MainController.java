ec9989e10dee4fa4e164d4de563edb3ed8145ce4
chdmtm@gmail.com <chdmtm@gmail.com>
2022-05-17 23:44:11 +0700

    add live filtering

--- src/main/java/com/example/komandor/controller/MainController.java
+++ src/main/java/com/example/komandor/controller/MainController.java
@@ -5,9 +5,11 @@ import com.example.komandor.service.GoodService;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
 import lombok.RequiredArgsConstructor;
@@ -27,6 +29,9 @@ public class MainController {
     public Text header;
 
     @FXML
+    public TextField searchBox;
+
+    @FXML
     private TableView<ProductDto> productTable;
 
     @FXML
@@ -43,19 +48,26 @@ public class MainController {
 
     @FXML
     public void initialize() {
-        List<ProductDto> productDtoList = goodService.getGoodInfoList().stream().map(e -> ProductDto.builder()
-                .goodId(new SimpleStringProperty(e.getId()))
-                .name(new SimpleStringProperty(e.getName()))
-                .description(new SimpleStringProperty(e.getDescription()))
-                .price(new SimpleStringProperty(e.getPrice()))
-                .build()).collect(Collectors.toList());
+        List<ProductDto> productDtoList = goodService.getGoodInfoList().stream().map(e -> ProductDto.builder().goodId(new SimpleStringProperty(e.getId())).name(new SimpleStringProperty(e.getName())).description(new SimpleStringProperty(e.getDescription())).price(new SimpleStringProperty(e.getPrice())).build()).collect(Collectors.toList());
 
-        ObservableList<ProductDto> productDtoObservableList = FXCollections.observableArrayList(productDtoList);
+        FilteredList<ProductDto> filterItems = new FilteredList<>(FXCollections.observableArrayList(productDtoList));
 
         goodId.setCellValueFactory(new PropertyValueFactory<>("GoodId"));
         name.setCellValueFactory(new PropertyValueFactory<>("Name"));
         description.setCellValueFactory(new PropertyValueFactory<>("Description"));
         price.setCellValueFactory(new PropertyValueFactory<>("Price"));
-        productTable.setItems(productDtoObservableList);
+        productTable.setItems(filterItems);
+
+        searchBox.textProperty().addListener((observable, oldValue, newValue) -> productTable.setItems(filterList(productDtoList, newValue.toLowerCase())));
+    }
+
+    private ObservableList<ProductDto> filterList(List<ProductDto> productDtos, String searchText) {
+        List<ProductDto> filteredList = productDtos.stream().filter(e -> searchFindsProduct(e, searchText)).collect(Collectors.toList());
+
+        return FXCollections.observableList(filteredList);
+    }
+
+    private boolean searchFindsProduct(ProductDto productDto, String searchText) {
+        return (productDto.getName().toLowerCase().contains(searchText.toLowerCase()));
     }
 }
