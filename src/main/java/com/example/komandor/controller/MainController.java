b2d924c810d6f25291a766319fc369df05648d7a
chdmtm@gmail.com <chdmtm@gmail.com>
2022-05-19 17:14:40 +0700

    add save order with alert

--- src/main/java/com/example/komandor/controller/MainController.java
+++ src/main/java/com/example/komandor/controller/MainController.java
@@ -1,8 +1,11 @@
 package com.example.komandor.controller;
 
+import com.example.komandor.dto.GoodOrderDto;
+import com.example.komandor.dto.OrderDto;
 import com.example.komandor.model.BasketProductDto;
 import com.example.komandor.model.ProductDto;
 import com.example.komandor.service.GoodService;
+import com.example.komandor.service.OrderService;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
@@ -10,15 +13,14 @@ import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
 import lombok.RequiredArgsConstructor;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -28,6 +30,8 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class MainController {
     private final GoodService goodService;
+
+    private final OrderService orderService;
 
     private final ObservableList<BasketProductDto> basketProductDtoObservableList = FXCollections.observableArrayList();
 
@@ -51,6 +55,18 @@ public class MainController {
 
     @FXML
     public TableColumn<String, BasketProductDto> basketPrice;
+
+    @FXML
+    public TableColumn<String, BasketProductDto> basketTotalPrice;
+
+    @FXML
+    public Text totalPayment;
+
+    @FXML
+    public Button paymentButton;
+
+    @FXML
+    public TextField paymentSum;
 
     @FXML
     private TableView<ProductDto> productTable;
@@ -85,6 +101,7 @@ public class MainController {
         basketName.setCellValueFactory(new PropertyValueFactory<>("BasketName"));
         basketCount.setCellValueFactory(new PropertyValueFactory<>("BasketCount"));
         basketPrice.setCellValueFactory(new PropertyValueFactory<>("BasketPrice"));
+        basketTotalPrice.setCellValueFactory(new PropertyValueFactory<>("BasketTotalPrice"));
 
         basketTable.setItems(basketProductDtoObservableList);
     }
@@ -101,12 +118,24 @@ public class MainController {
 
     public void addBasketRow(ActionEvent actionEvent) {
         ProductDto selectedItem = productTable.getSelectionModel().getSelectedItem();
-        Optional<BasketProductDto> optionalBasketProductDto = basketProductDtoObservableList.stream()
-                .filter(e -> e.getBasketGoodId().equals(selectedItem.getGoodId())).findFirst();
+        Optional<BasketProductDto> optionalBasketProductDto = basketProductDtoObservableList.stream().filter(e -> e.getBasketGoodId().equals(selectedItem.getGoodId())).findFirst();
 
         if (optionalBasketProductDto.isPresent()) {
-            optionalBasketProductDto.ifPresent(e -> e.setBasketCount(e.getBasketCount() + 1));
+            optionalBasketProductDto.ifPresent(e -> {
+                int goodCount = e.getBasketCount() + 1;
+                e.setBasketCount(goodCount);
+                BigDecimal totalSum = BigDecimal.valueOf(Double.parseDouble(e.getBasketPrice())).multiply(BigDecimal.valueOf(goodCount));
+                e.setBasketTotalPrice(totalSum.toString());
+            });
+
             basketTable.refresh();
+
+            BigDecimal orderSum = basketProductDtoObservableList.stream()
+                    .map(e -> BigDecimal.valueOf(Double.parseDouble(e.getBasketTotalPrice())))
+                    .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+            totalPayment.setText(orderSum.toString());
+
             return;
         }
 
@@ -117,6 +146,49 @@ public class MainController {
                 .basketPrice(new SimpleStringProperty(selectedItem.getPrice()))
                 .basketTotalPrice(new SimpleStringProperty(selectedItem.getPrice())).build());
 
+        BigDecimal orderSum = basketProductDtoObservableList.stream()
+                .map(e -> BigDecimal.valueOf(Double.parseDouble(e.getBasketTotalPrice())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        totalPayment.setText(orderSum.toString());
+
         basketTable.refresh();
+    }
+
+    public void clearBasket(ActionEvent actionEvent) {
+        basketProductDtoObservableList.clear();
+        totalPayment.setText("");
+    }
+
+    public void doPayment(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
+        alert.setHeaderText("Info");
+        alert.setTitle("Notice");
+
+        String text = paymentSum.getText();
+
+        if (text.equals(totalPayment.getText())) {
+            List<GoodOrderDto> goodOrderDtos = basketProductDtoObservableList.stream()
+                    .map(e -> GoodOrderDto.builder()
+                            .id(e.getBasketGoodId())
+                            .count(e.getBasketCount()).build())
+                    .collect(Collectors.toList());
+
+            OrderDto orderDto = OrderDto.builder().goodOrderDtoList(goodOrderDtos).payment(BigDecimal.valueOf(Double.parseDouble(text))).build();
+
+            orderService.createCheck(orderDto);
+
+            alert.setContentText("Создан заказ на сумму " + text);
+            alert.show();
+            basketProductDtoObservableList.clear();
+            paymentSum.setText("");
+            totalPayment.setText("");
+            return;
+        }
+
+        alert.setContentText("введено неверное значение оплаты");
+
+        alert.show();
     }
 }
